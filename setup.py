56655c6525e3494ad5832e7c4d07716b9b28dd33
Ben Busby <contact@benbusby.com>
2025-02-27 16:33:05 -0700

    Use new swig parser header name in manifest

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="BSTPythonSDK",
-    version="3.22.0.dev1",
+    version="3.22.0.dev2",
     author="Black Swift Technologies",
     author_email="ben.busby@blackswifttech.com",
     description="BST Flight Management SDK",
