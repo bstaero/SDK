d79022416af3707235b0e53e779131db4a613a48
Jack Elston <elstonj@blackswifttech.com>
2025-07-25 00:15:02 -0600

    - adding testing program for PSNS board

---
+++ examples/psns_test/test_handler.h
@@ -0,0 +1,23 @@
+/*=+--+=#=+--         SwiftCore Flight Management Software        --+=#=+--+=#*\
+|               Copyright (C) 2015 Black Swift Technologies LLC.               |
+|                             All Rights Reserved.                             |
+
+     NOTICE:  All information contained herein is, and remains the property 
+     of Black Swift Technologies.
+
+     The intellectual and technical concepts contained herein are 
+     proprietary to Black Swift Technologies LLC and may be covered by U.S. 
+     and foreign patents, patents in process, and are protected by trade 
+     secret or copyright law.
+
+     Dissemination of this information or reproduction of this material is 
+     strictly forbidden unless prior written permission is obtained from 
+     Black Swift Technologies LLC.
+|                                                                              |
+|                                                                              |
+\*=+--+=#=+--                 --+=#=+--+=#=+--                    --+=#=+--+=#*/
+#ifndef _TEST_HANDLER_H_
+#define _TEST_HANDLER_H_
+
+
+#endif
