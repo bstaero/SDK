2ae3f7c0acfa94c7ca6032854be678221a893336
Jack Elston <elstonj@blackswifttech.com>
2026-03-05 11:31:30 -0700

    - updated bridge to deal with nan for rh and temp - updated bst protocol to remove unnecessary double logging of comms   packets - mupdated flight plan module for bug in state machine that was causing   issues with timeouts

--- src/bst_can/bridge.c
+++ src/bst_can/bridge.c
@@ -1,21 +1,19 @@
-/*=+--+=#=+--            SwiftPilot Autopilot Software            --+=#=+--+=#*\
+/*=+--+=#=+--         SwiftCore Flight Management Software        --+=#=+--+=#*\
 |               Copyright (C) 2012 Black Swift Technologies LLC.               |
 |                             All Rights Reserved.                             |
 
-     This program is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License version 2 as
-     published by the Free Software Foundation.
+     NOTICE:  All information contained herein is, and remains the property
+     of Black Swift Technologies.
 
-     This program is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
+     The intellectual and technical concepts contained herein are
+     proprietary to Black Swift Technologies LLC and may be covered by U.S.
+     and foreign patents, patents in process, and are protected by trade
+     secret or copyright law.
 
-     You should have received a copy of the GNU General Public License
-     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-                                  Jack Elston                                   
-|                          elstonj@blackswifttech.com                          |
+     Dissemination of this information or reproduction of this material is
+     strictly forbidden unless prior written permission is obtained from
+     Black Swift Technologies LLC.
+|                                                                              |
 |                                                                              |
 \*=+--+=#=+--                 --+=#=+--+=#=+--                    --+=#=+--+=#*/
 
@@ -640,16 +638,16 @@ void BRIDGE_HandleAirDataPkt(uint8_t *byte, uint8_t size)
 	float t0 = getElapsedTime();
 
 #if !defined BOARD_MHP && !defined SEPARATE_AIRDATA && !defined STANDALONE_BUILD
-	if(data->static_pressure > -FLT_MAX) {
+	if(isnan(data->static_pressure) || data->static_pressure > -FLT_MAX) {
 		updateStaticPressure(t0, data->static_pressure, data->air_temperature);
 	}
-	if(data->dynamic_pressure > -FLT_MAX) {
+	if(isnan(data->dynamic_pressure) || data->dynamic_pressure > -FLT_MAX) {
 		updateDynamicPressure(t0, data->dynamic_pressure, data->air_temperature);
 	}
-	if(data->air_temperature > -FLT_MAX) {
+	if(isnan(data->air_temperature) || data->air_temperature > -FLT_MAX) {
 		updateTemperature(t0, data->air_temperature);
 	}
-	if(data->humidity > -FLT_MAX) {
+	if(isnan(data->humidity) || data->humidity > -FLT_MAX) {
 		updateHumidity(t0, data->humidity);
 	}
 
